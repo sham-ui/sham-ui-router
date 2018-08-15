8b3364fe66a5593d4f52833d269ba1da7b74cc98
Eugeny Burnashov <shamcode@gmail.com>
2018-08-15 21:34:30 +0700

    Refactoring with sham-ui-directives https://github.com/sham-ui/sham-ui-router/issues/2

--- src/widgets/LinkTo.js
+++ src/widgets/LinkTo.js
@@ -1,83 +1,57 @@
 import { DI, inject, options } from 'sham-ui';
-import LinkToWidget from './LinkTo.sht';
+import directives from 'sham-ui-directives';
+import LinkToWidgetTemplate from './LinkTo.sht';
 
-export default class extends LinkToWidget {
+export default class LinkToWidget extends LinkToWidgetTemplate {
+    constructor( selector, id, options ) {
+        super( selector, id, {
+            ...options,
+            directives: {
+                onclick: directives.onclick,
+                ...options.directives,
+            }
+        } );
+    }
+
     @inject router = 'router';
 
-    @options
-    get text() {
-        return '';
-    }
-
-    @options
-    get params() {
-        return {}
-    }
-
-    @options
-    get useActiveClass() {
-        return false;
-    }
-
-    @options
-    get activeClass() {
-        return 'active';
-    }
-
-    @options
-    get className() {
-        return '';
-    }
+    @options get text() { return ''; }
+    @options get params() { return {} }
+    @options get useActiveClass() { return false; }
+    @options get activeClass() { return 'active'; }
+    @options get className() { return ''; }
 
     generateURL( path, params ) {
         return this.lastGeneratedURL = this.router.generate( path, params );
     }
-
     isActive( path, params ) {
         return this.lastGeneratedURL === this.router.lastRouteResolved().url;
     }
-
     _registryInRouter() {
         if ( this.options.useActiveClass ) {
             this.router._registerActivePageLink( this );
         }
     }
-
     _removeFromRouter() {
         if ( this.options.useActiveClass ) {
             this.router._unregisterActivePageLink( this );
         }
     }
-
     get aNode() {
         return this.querySelector( 'a' );
     }
-
     update() {
         super.update( ...arguments );
         this._registryInRouter();
     }
-
-    bindEvents() {
-        this.__bindedClick = this._click.bind( this );
-        this.aNode.addEventListener(
-            'click',
-            this.__bindedClick
-        )
-    }
-
     destroy() {
-        this.aNode.removeEventListener( 'click', this.__bindedClick );
         this._removeFromRouter();
     }
-
     remove() {
-        this.aNode.removeEventListener( 'click', this.__bindedClick );
         super.remove( ...arguments );
         this._removeFromRouter();
     }
-
-    _click( e ) {
+    click( e ) {
         e.preventDefault();
         this.router.navigate( this.aNode.getAttribute( 'href' ) );
     }
