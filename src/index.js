f76c0e54091f44fe26ed7af902b582323885065f
Eugeny Burnashov <shamcode@gmail.com>
2019-11-19 19:26:18 +0700

    Add some docs

--- src/index.js
+++ src/index.js
@@ -2,15 +2,98 @@ import { DI } from 'sham-ui';
 import { storage } from './storage';
 import Navigo from 'navigo';
 
+
+/**
+ * Router service
+ * @example
+ * import FooPage from '../components/FooPage.sht';
+ * import BarPage from '../components/BarPage.sht';
+ * import Router from 'sham-ui-router';
+ *
+ * const router = new Router();
+ * router
+ *     .bindPage(
+ *         '/foo', // URL
+ *         'foo', // Name
+ *         FooPage, // Component class
+ *         { componentOption: 1 } // Component options
+ *     )
+ *     .bindPage( '/bar/:some_param/detail', 'bar', BarPage, {} )
+ *     .resolve();
+ */
 export default class Router {
+
+    /**
+     * @param {string|null} [root=null] Root URL
+     * @param {boolean} [useHash=false] Use hash symbol as delimiter
+     * @param {string} [hash='#'] Hash symbol (use useHash=true)
+     */
     constructor( root = null, useHash = false, hash = '#' ) {
         DI.bind( 'router', this );
+        /**
+         * @type {RouterStorage}
+         */
         this.storage = storage;
         this.router = new Navigo( root, useHash, hash );
         this._initProxyMethods();
     }
 
+
+    /**
+     * @private
+     */
     _initProxyMethods() {
+
+        /**
+         * Resolve current url & run router
+         * @memberof Router
+         * @method
+         * @name resolve
+         */
+
+        /**
+         * Not found handler
+         * @memberof Router
+         * @method
+         * @name notFound
+         * @see https://github.com/krasimir/navigo#not-found-handler
+         */
+
+        /**
+         * Changing the page
+         * @memberof Router
+         * @method
+         * @name navigate
+         * @param {string} url Destination url
+         */
+
+        /**
+         * Hooks
+         * @memberof Router
+         * @method
+         * @name hooks
+         * @param {Object} hooks Object with hooks
+         * @see https://github.com/krasimir/navigo#hooks
+         */
+
+        /**
+         * Generate url for page
+         * @memberof Router
+         * @method
+         * @name generate
+         * @param {string} name
+         * @param {Object} [params]
+         * @see https://github.com/krasimir/navigo#named-routes
+         * @example
+         * router
+         *     .bindPage( '/trip/:tripId/edit', 'trip.edit', PageComponent, {} )
+         *     .bindPage( '/trip/save', 'trip.save', PageComponent, {} )
+         *     .bindPage( '/trip/:action/:tripId', 'trip.action', PageComponent, {} );
+         * console.log(router.generate('trip.edit', { tripId: 42 })); // --> /trip/42/edit
+         * console.log(router.generate('trip.action', { tripId: 42, action: 'save' })); // --> /trip/save/42
+         * console.log(router.generate('trip.save')); // --> /trip/save
+         */
+
         [
             'resolve',
             'on',
@@ -34,6 +117,14 @@ export default class Router {
         } );
     }
 
+    /**
+     * Bind page component & url
+     * @param {string} url Url for page
+     * @param {string} name Page name
+     * @param {Class<Component>} pageComponent Component for page
+     * @param {Object} componentOptions Options for component
+     * @return {Router}
+     */
     bindPage( url, name, pageComponent, componentOptions ) {
         this.on( url, {
             as: name,
--- src/storage.js
+++ src/storage.js
@@ -1,6 +1,19 @@
 import createStorage from 'sham-ui-data-storage';
+/**
+ * Data storage for router service
+ * @see https://github.com/sham-ui/sham-ui-data-storage
+ * @typedef {Object} RouterStorage
+ * @property {string} url Current page url
+ * @property {string} name Current page name
+ * @property {Object} params Current page params
+ * @property {Class<Component>} activePageComponent Current page component class
+ * @property {Object} activePageOptions Options for current page component
+ */
+
 export const { storage, useStorage } = createStorage( {
     url: '',
+    name: '',
+    params: {},
     activePageComponent: null,
     activePageOptions: null
 }, { DI: 'router:storage' } );
